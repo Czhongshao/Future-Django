a82eb2e01ddf7f1ebc1e25f26973ac5e0fa08d7d
zhongshao <145018310+Czhongshao@users.noreply.github.com>
2025-03-27 21:01:15 +0800

    删除无用文档

--- FutureDjango/web/views.py
+++
@@ -1,7 +0,0 @@
-# web/views.py
-from django.shortcuts import render
-from web import models
-
-def population_list(request):
-    population_queryset = models.Population.objects.all()
-    return render(request, "sheet1.html", {"population_queryset": population_queryset})
